ad408702240af8cc84eb257c5bea777e07f5805c
Taj Loodu <tajloodu@gmail.com>
2020-09-04 16:27:15 -0400

    created styled menu

--- tajloodu-site/src/components/header.js
+++ tajloodu-site/src/components/header.js
@@ -27,6 +27,49 @@ const StyledContentWrapper = styled(ContentWrapper)`
   }
 `
 
+const StyledMenu = styled.button`
+  z-index: 12;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  width: 2rem;
+  height: 2rem;
+  background: transparent;
+  border: none;
+  cursor; pointer;
+  padding: 0;
+
+  &:focus {
+    outline:none;
+  }
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.lg}) {
+    display: none;
+  }
+
+  div {
+    width: 2rem;
+    height: 0.25rem;
+    background: ${({ theme }) => theme.colors.primary};
+    border-radius: 0.625rem;
+    transition: all 0.3s ease-in-out;
+    position; relative;
+    transform-origin: 1px;
+
+    :first-child {
+      transform: ${({ open }) => (open ? "rotate(45deg)" : "rotate(0)")};
+    }
+
+    :nth-child(2) {
+      opacity: ${({ open }) => (open ? "rotate(45deg)" : "rotate(0)")};
+    }
+
+    :nth-child(3) {
+      transform: ${({ open }) => (open ? "rotate(-45deg)" : "rotate(0)")};
+    }
+  }
+`
+
 const Header = ({ siteTitle }) => (
   <header
     style={{
