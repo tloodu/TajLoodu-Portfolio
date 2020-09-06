dfb516f21f4afea51739dc7b386f40fd816d69b7
Taj Loodu <tajloodu@gmail.com>
2020-09-06 15:41:00 -0400

    initialised header

--- tajloodu-site/src/components/header.js
+++ tajloodu-site/src/components/header.js
@@ -5,22 +5,22 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
+      background: `white`,
+      marginBottom: `2.0rem`,
     }}
   >
     <div
       style={{
         margin: `0 auto`,
         maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        padding: `2.0rem 1.0875rem`,
       }}
     >
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
-            color: `white`,
+            color: `black`,
             textDecoration: `none`,
           }}
         >
