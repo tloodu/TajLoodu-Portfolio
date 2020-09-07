e96a0e954ae7f9b26b8ce9f4bf9f6ed7f79e252c
Taj Loodu <tajloodu@gmail.com>
2020-09-07 11:43:13 -0400

    created layout file and applied it to index page

--- tajloodu-site/src/pages/index.js
+++ tajloodu-site/src/pages/index.js
@@ -1,7 +1,13 @@
 import React from "react"
 
+import Layout from "../components/layout"
+
 const IndexPage = () => {
-  return <h1>Hello</h1>
+  return (
+    <Layout>
+      Hello
+    </Layout>
+  )
 }
 
 export default IndexPage
